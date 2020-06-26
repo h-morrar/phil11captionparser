efc1840e0ddebf24dfe1686e56eea7e7fa452784
Hilal Morrar <hamorrar@ucsc.edu>
2020-06-25 19:41:31 -0700

    works!

--- parser.py
+++ parser.py
@@ -6,14 +6,16 @@ def main():
     originalFileObj = open(r"%s.txt" % originalFileName, "r")
 
     newFileName = originalFileName + "-parsed"
-    newFileObj = open (r"%s.txt" % newFileName, "w")
+    newFileObj = open(r"%s.txt" % newFileName, "a")
 
-    Lines = originalFileObj.readLines()
+    Lines = originalFileObj.readlines()
 
     for line in Lines:
         if (line[0].isdigit() and (line[1].isdigit()) and (line[2] == ":") and (line[3].isdigit()) and (line[4].isdigit())):
             continue
+        elif (line.strip() == ""):
+            continue
         else:
-            
+            newFileObj.write(line.strip() + " ")
 
 main()
