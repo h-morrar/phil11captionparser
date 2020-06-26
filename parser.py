c315743c8fd37408a332d1e93065da95aa49c91b
Hilal Morrar <hamorrar@ucsc.edu>
2020-06-25 19:19:23 -0700

    made conditional for timestamps in input

--- parser.py
+++ parser.py
@@ -1,9 +1,19 @@
 def main():
+
     lectureNum = "1"
+
     originalFileName = "lecture-" + lectureNum
     originalFileObj = open(r"%s.txt" % originalFileName, "r")
 
     newFileName = originalFileName + "-parsed"
     newFileObj = open (r"%s.txt" % newFileName, "w")
+
+    Lines = originalFileObj.readLines()
+
+    for line in Lines:
+        if (line[0].isdigit() and (line[1].isdigit()) and (line[2] == ":") and (line[3].isdigit()) and (line[4].isdigit())):
+            continue
+        else:
+            
 
 main()
