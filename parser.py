656904d0f90db14fe20fc0e58dfdf908433fbcae
Hilal Morrar <hamorrar@ucsc.edu>
2020-06-25 18:53:51 -0700

    opened original and created new file

--- parser.py
+++ parser.py
@@ -1,4 +1,9 @@
 def main():
-    lectureNum = 1
+    lectureNum = "1"
     originalFileName = "lecture-" + lectureNum
-    originalFile = open(r"")
+    originalFileObj = open(r"%s.txt" % originalFileName, "r")
+
+    newFileName = originalFileName + "-parsed"
+    newFileObj = open (r"%s.txt" % newFileName, "w")
+
+main()
